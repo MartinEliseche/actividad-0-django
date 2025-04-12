14163e3c120d45920ef9457b961b8cd9b97097dd
MartinEliseche <tinchoeliseche@gmail.com>
2025-04-12 19:51:05 -0300

    [Parte 8.1] Agregar Django Debug Toolbar.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,3 +1,6 @@
+import debug_toolbar
+
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,3 +8,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls))
+    ]
